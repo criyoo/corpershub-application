4189c5b92caeb55cd329fc5fe28f486ae2727956
Christian Aluya <criyo.career@gmail.com>
2026-07-01 15:09:34 +0100

    Initial Commit

--- apps/api/apps/subscriptions/constants.py
+++ apps/api/apps/subscriptions/constants.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 # Subscription plan prices in kobo (100 kobo = 1 Naira)
 SUBSCRIPTION_PLAN_PRICES = {
     "free": 0,
-    "three-months": 30000,  # N300
-    "six-months": 50000,  # N500
-    "twelve-months": 70000,  # N700
+    "three-months": 1000000,  # N10000
+    "six-months": 1800000,  # N18000
+    "twelve-months": 3000000,  # N30000
 }
